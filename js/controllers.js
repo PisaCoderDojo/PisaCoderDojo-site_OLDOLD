d62c22d4d30f75dda1af00ecbdb57410625783b1
lucarin91 <lucarin91@gmail.com>
2015-02-25 06:55:52 -0800

    add administration wiith add and delete

--- js/controllers.js
+++ js/controllers.js
@@ -7,23 +7,6 @@ coderDojoControllers.controller('homeCtrl', ['$scope',
     $scope.test="sei in Home!";
   }]);
 
-/*
-newsCtrl.resolve = {
-  news: function($q, $http){
-    var deferred = $q.defer();
-
-    $http.get('php/getNews.php').
-      success(function(data, status, headers, config) {
-        deferred.resolve(data)
-        console.log(data);
-      }).
-      error(function(data, status, headers, config) {
-          console.log('error');
-      });
-    return deferred.promise;
-  }
-};
-*/
 coderDojoControllers.controller('newsCtrl', ['$scope', 'news',
   function($scope, news) {
     $scope.news = angular.fromJson(news.data);
