a3e07156e36f61d1a82447b921d0b45942a0188c
lucarin91 <lucarin91@gmail.com>
2015-03-11 01:14:31 +0100

    implement contact form

--- php/sendMail.php
+++ php/sendMail.php
@@ -1,16 +1,17 @@
 <?php
   $data = json_decode(file_get_contents("php://input"));
 
-  $to = "lucarin91@gmail.com";
-  $subject = $data->subject;
-  $message = $data->text;
+  $to = "pisa.it@coderdojo.com";
+  $subject = "[contact-form] " . $data->subject;
+  $message = "From: ". $data->mail . " <br/><br/> " . $data->text;
 
   // Always set content-type when sending HTML email
   $headers = "MIME-Version: 1.0" . "\r\n";
   $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
   // More headers
-  $headers .= "From: <" . $data->mail . ">\r\n";
+  $headers .= "From: contact-form <" . $data->mail . ">\r\n";
+  //$headers .= "Cc: " . $data->mail . "\r\n";
   //$headers .= 'Cc: myboss@example.com' . "\r\n";
 
   $ris = mail($to,$subject,$message,$headers);
