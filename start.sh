e34c05f3db4ca51cf482faf2db34b7e23779cbf5
lucarin91 <lucarin91@gmail.com>
2016-02-02 23:48:51 +0100

    add mentors

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ docker build -t webapp . # Build a Docker image named "webapp" from this locatio
 # wait for it to build...
 
 # Run the docker container
-docker run -t -v `pwd`:/var/www:rw -p 8080:8080 webapp /sbin/my_init
+docker run -t -i -v `pwd`:/var/www:rw -p 8080:8080 webapp /sbin/my_init
